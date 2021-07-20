6f56e68b811aea8e9a3376616f12d9c87f36973f
HalaMesallam <87154339+HalaMesallam@users.noreply.github.com>
2021-07-20 22:19:41 +0200

    adding instance variables in the product page

--- ProductPage.java
+++ ProductPage.java
@@ -3,5 +3,35 @@
  */
 
 public class ProductPage {
+	private double Shippingcosts;
+	private String color;
+	private boolean Productavailability;
+	
+	
+	private double getShippingcosts() {
+		return Shippingcosts;
+	}
+	
+	private void setShippingcosts(double shippingcosts) {
+		Shippingcosts = shippingcosts;
+	}
+
+	private String getColor() {
+		return color;
+	}
+
+	private void setColor(String color) {
+		this.color = color;
+	}
+
+	private boolean isProductavailability() {
+		return Productavailability;
+	}
+
+	private void setProductavailability(boolean productavailability) {
+		Productavailability = productavailability;
+	}
+	
+
 
 }
