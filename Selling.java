3ce7dd157eb6c59a0e4b8b53945a2afe524e2daf
TenzinDorjee1 <87152720+TenzinDorjee1@users.noreply.github.com>
2021-07-21 12:00:39 -0600

    updated selling to remove print and scanner statements

--- Selling.java
+++ Selling.java
@@ -14,35 +14,7 @@ public class Selling {
 	private String color;
 	private String datePosted;
 
-	public static void main(String[] args) {
-		
-		System.out.println("Would you like to sell and item? (yes/no)");
-		Scanner keyboard = new Scanner(System.in);
-		String sellUserInput = keyboard.nextLine();
-		if (sellUserInput.equalsIgnoreCase("yes")){
-			
-			System.out.println("What is the name of the item?");
-			String sellItemName = keyboard.nextLine();
-			
-			System.out.println("What is the price of the item?");
-			String sellItemPrice = keyboard.nextLine();
-			
-			System.out.println("What is the category of the item?");
-			String sellItemCat = keyboard.nextLine();
-			
-			System.out.println("what is the size of the item (XS,S,M,L,XL or N/A)");
-			String sellItemSize = keyboard.nextLine();
-			
-			System.out.println("what is the color of the item");
-			String sellItemColor = keyboard.nextLine();	
-			
-			System.out.println("what is the date posted of the item (ex.2021/07/23)");
-			String sellItemdatePosted = keyboard.nextLine();	
-			
-			float sellItemPriceFloat = Float.parseFloat(sellItemPrice);
-			Selling sellingItem = new Selling(sellItemName, sellItemPriceFloat, sellItemCat, sellItemSize, sellItemColor, sellItemdatePosted);
-		}	
-	}
+
 
 public Selling(String name, float sellItemPrice, String category, String size, String color, String datePosted) {
 
@@ -52,20 +24,19 @@ public Selling(String name, float sellItemPrice, String category, String size, S
 	this.color = color;
 	this.datePosted = datePosted;
 	
-	System.out.println(sellItemPrice);
-	System.out.println(category);
-	System.out.println(size);
-	System.out.println(color);
-	System.out.println(datePosted);
+	System.out.println(price);
 	
 }
 
 public String getName(){
+	System.out.println(name);
 	return name;
 }
 
 public void setName(String name) {
+	
 	this.name = name;
+	
 }
 
 public float getPrice(){
