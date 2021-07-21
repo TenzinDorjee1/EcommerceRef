c2c66f513e5d487642f8558c226bc69daca73df3
TenzinDorjee1 <87152720+TenzinDorjee1@users.noreply.github.com>
2021-07-21 12:00:09 -0600

    created a test file

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester extends HomePage{
+	public static void main(String args[]) {
+		System.out.println("Welcome to our Ecommerce Application!");
+		HomePage UserTestIn = new HomePage();
+		UserTestIn.HomeOptions();
+	}
+}
+
+
