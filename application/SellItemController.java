25a9cbad37e4cca8c6e3a1f804618e09e33ffe87
TenzinDorjee1 <87152720+TenzinDorjee1@users.noreply.github.com>
2021-08-04 20:24:25 -0600

    sell product data working

--- application/SellItemController.java
+++ application/SellItemController.java
@@ -5,11 +5,19 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import classes.*;
 
 public class SellItemController {
 
     @FXML
     private TextField sizefield;
+
+    @FXML
+    private Label itemCat;
+
+    @FXML
+    private Label itemColor;
 
     @FXML
     private TextField datefield;
@@ -18,22 +26,16 @@ public class SellItemController {
     private Button productbutoon;
 
     @FXML
-    private Label Namelabel;
-
-    @FXML
-    private Label CatLabel;
-
-    @FXML
     private TextField colorfield;
-
-    @FXML
-    private Label Datelabel;
 
     @FXML
     private TextField Catfield;
 
     @FXML
-    private Label pricelabel;
+    private Label itemDatePosted;
+
+    @FXML
+    private Label itemName;
 
     @FXML
     private TextField Namefield;
@@ -42,27 +44,42 @@ public class SellItemController {
     private TextField pricefield;
 
     @FXML
-    private Label sizelabel;
+    private Label itemPrice;
 
     @FXML
     private Button resetbutton;
 
     @FXML
-    private Label colorlabel;
+    private Label itemSize;
 
     @FXML
-    void clearitem(ActionEvent event) {
-
+    
+    void keyTyped(KeyEvent event) {
+    	
+    	
     }
 
     @FXML
     void clickproductpage(ActionEvent event) {
-
+    	String name = Namefield.getText();
+    	String price = pricefield.getText();
+    	String category = Catfield.getText();
+    	String size = sizefield.getText();
+    	String color = colorfield.getText();
+    	String datePosted = datefield.getText();
+    	
+    	Item ecommItem = new Item(name, price, category, size, color, datePosted);
+    	ItemsList ecommProduct = new ItemsList();
+    	
+    	ecommProduct.setAddItem(ecommItem);
+    	ecommProduct.displayItem();
+    	
+    	
+    	
     }
 
-    @FXML
-    void da1212(ActionEvent event) {
-
-    }
+   
+ 
 
 }
+
